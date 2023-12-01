b7d0e508e75e50302abd5f2a6315c3958ef371a1
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2023-12-01 11:14:14 +0100

    added an automatically-triggered help option

--- client.go
+++ client.go
@@ -105,7 +105,23 @@ func main() {
 	getPeerAddressesFlag := flag.String("getAddresses", "", "specify a peer ID to get the addresses from")
 	getPeerKeyFlag := flag.String("getKey", "", "specify a peer ID to get the pubkey from")
 	getPeerRootHashFlag := flag.String("getRootHash", "", "specify a peer ID to get the root hash from")
+	helpFlag := flag.Bool("help", false, "use to get command usage")
+	// TODO : allow a list of peers instead of a single one here
 	flag.Parse()
+	if !(*helpFlag || *listPeersFlag || *getPeerAddressesFlag != "" || *getPeerKeyFlag != "" || *getPeerRootHashFlag != "") {
+		*helpFlag = true // default behavior
+	}
+	if *helpFlag {
+		fmt.Println("Usage : go run client.go [options]")
+		fmt.Println("Several options can be used, but the help option will override any other and is enabled by default.")
+		fmt.Println("Options :")
+		fmt.Println("-list / -list=true : fetches and displays a list of known peers from the server. Disabled by default.")
+		fmt.Println("-help / -help=true : displays this help and exits. Enabled by default. Overrides any other option.")
+		fmt.Println("-getAddresses=[peer_name] : fetches and displays a list of known addresses for a given peer, from the server. Disabled by default.")
+		fmt.Println("-getKey=[peer_name] : fetches and displays the public key of a given peer, from the server. Disabled by default.")
+		fmt.Println("-getRootHash=[peer_name] : fetches and displays the hash of the root of a given peer, from the server. Disabled by default.")
+		return
+	}
 	if *listPeersFlag {
 		req := buildGetPeersRequest()
 		if req != nil {
