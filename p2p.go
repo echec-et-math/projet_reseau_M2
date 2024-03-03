1012e6c48c54f3edd695bb6cff5fedc92363f8e8
Ulysse DUPONT-LATAPIE <ulysseh2o@free.fr>
2024-03-03 16:48:10 +0100

    better writing for compareHash

--- p2p.go
+++ p2p.go
@@ -351,17 +351,15 @@ func verifChunk(content []byte, Hash []byte) bool {
 }
 
 func compareHash(h1 []byte, h2 []byte) bool {
-	if len(h1) == len(h2) {
-		for i := 0; i < len(h1); i++ {
-			if h1[i] != h2[i] {
-				return false
-			}
-		}
-		return true
-	} else {
-
+	if len(h1) != len(h2) {
 		return false
 	}
+	for i := 0; i < len(h1); i++ {
+		if h1[i] != h2[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func findNode(Hash []byte, n Node) *Node {
